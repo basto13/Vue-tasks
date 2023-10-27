b92b89eeef55577609712ead5b5414947a4674c2
Olga Lushnikova <63054910+basto13@users.noreply.github.com>
2023-10-27 15:50:10 +0200

    add ProductDetails micro-project

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <ProductDetails />
+</template>
+
+<script>
+import ProductDetails from "./components/ProductDetails.vue";
+
+export default {
+  name: "App",
+  components: { ProductDetails },
+};
+</script>
+
+<style></style>
---
+++ my-vue-app/src/components/ProductDetails.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{ product.name }}</h1>
+    <p>Price: {{ formattedPrice }}</p>
+    <p>{{ product.available ? "Available" : "Out of stock" }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      product: {
+        name: "Sample Product",
+        price: 99.99,
+        available: true,
+      },
+    };
+  },
+  computed: {
+    formattedPrice() {
+      const price = this.product.price;
+      return `$${price.toFixed(2)}`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
